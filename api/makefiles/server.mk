ee65ffd75fb005ff9b4c8e5107ef48cbe64257c6
Daniel <danielmapar@gmail.com>
2019-08-07 04:04:53 -0400

    Working docker file

---
+++ api/makefiles/server.mk
@@ -0,0 +1,20 @@
+### SERVER
+# ¯¯¯¯¯¯¯¯¯¯¯
+
+server.start: ## Start server in its docker container
+	docker-compose up server
+
+server.bash: ## Connect to server to lauch commands
+	docker-compose exec server bash
+
+server.daemon: ## Start daemon server in its docker container
+	docker-compose up -d server
+
+server.stop: ## Start server in its docker container
+	docker-compose stop
+
+server.logs: ## Display server logs
+	tail -f server.log
+
+server.upgrade: ## Upgrade pip dependencies
+	docker-compose run --rm server bash -c "python vendor/bin/pip-upgrade requirements.txt requirements-dev.txt --skip-virtualenv-check"
